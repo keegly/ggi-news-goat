8879697b7e38fae7ff1d752e6f3f178a069da8ab
Keegan Taylor <glove.save.30@gmail.com>
2017-09-16 16:47:12 +0000

    added meme status

--- bot.py
+++ bot.py
@@ -86,6 +86,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-------')
+    await client.change_presence(game=discord.Game(name='Shit Just Goat Real'))
 
 @client.event
 async def on_message(message):
