a474a3760a608cfeb6713019cc0d17a891ab7a7c
Keegan Taylor <glove.save.30@gmail.com>
2017-10-03 16:43:53 +0000

    added sasssy reactions to .news for non permitted users

--- bot.py
+++ bot.py
@@ -262,6 +262,9 @@ async def on_message(message):
         return
 
     if message.author.id not in ['236291672655396864', '354632104090271746', '354636345479528448']:
+        # add sassy reaction(s)
+        await client.add_reaction(message, '🙅')
+        await client.add_reaction(message, '🤔')
         return
 
     if message.content.startswith('.recent'):
